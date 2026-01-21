d70f2ec1f00b432d9f5c4fae2db1e9bdbb4598d6
Aryan Jain <coderaryanjain@gmail.com>
2026-01-21 07:56:26 +0530

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables instead of hardcoded values.

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-// Firebase imports
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-// --- Yahaan par Firebase config daalein ---
-// (Yeh keys aapke pichhle conversation se hain)
-const firebaseConfig = {
-  apiKey: "AIzaSyB5VDEo4ewfFQbhj0tqMPAnwhza2fPICe8",
-  authDomain: "zenithely.firebaseapp.com",
-  projectId: "zenithely",
-  storageBucket: "zenithely.firebasestorage.app",
-  messagingSenderId: "1079569948368",
-  appId: "1:1079569948368:web:de96220d769739c2d648d5",
-  measurementId: "G-ETT007M5LV"
-};
-
-// --- Firebase services ko initialize karein ---
-const app = initializeApp(firebaseConfig);
-
-// Services ko export karein taaki poori app use kar sake
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-=======
 // Firebase imports
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -45,5 +20,3 @@ const app = initializeApp(firebaseConfig);
 // Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
->>>>>>> 2456ec264d991bff9cb8d8ee3f6e135ecaf2b092
